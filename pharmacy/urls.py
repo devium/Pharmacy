cb1eac98980215b1ce2f69f92a91da6d7b94fbf7
Maxim Nikolsky <maxi.nikolsky@gmail.com>
2016-09-18 14:16:04 +0300

    Added help page

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import TemplateView
 from django.views.static import serve
 
 from products.views import HomePageView, CatalogueView, AddProductView
@@ -12,7 +13,7 @@ urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^add/(?P<pk>.*)/(?P<slug>.*)$', AddProductView.as_view(), name='add_product'),
     url(r'^catalogue/(?P<slug>.*)$', CatalogueView.as_view(), name='catalogue'),
-    # url(r'^help$', 'products.views.help', name='help'),
+    url(r'^help$', TemplateView.as_view(template_name='help.html'), name='help'),
     # url(r'^delivery_help$', 'products.views.delivery_help', name='delivery_help'),
     # url(r'^order_help$', 'products.views.order_help', name='order_help'),
     # url(r'^cart$', 'carts.views.cart', name='cart'),
